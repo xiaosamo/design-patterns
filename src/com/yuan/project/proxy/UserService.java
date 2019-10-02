4c54b5f22ae5d83fe4b394af688161f5323d36ef
e <yuan491187718@gmail.com>
2019-10-02 15:04:54 +0800

    代理模式

---
+++ src/com/yuan/project/proxy/UserService.java
@@ -0,0 +1,19 @@
+package com.yuan.project.proxy;
+
+/**
+ * @author yuan
+ * @date 2019/10/2
+ * @description
+ */
+public interface UserService {
+    /**
+     * 添加用户
+     */
+    void add();
+
+    /**
+     * 删除用户
+     */
+    void delete();
+
+}
